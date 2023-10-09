39fe34fb7d9f52d8bfe68cf778bf7d989c3158ca
Shunta Kainuma <arias230461@gmail.com>
2023-10-09 20:57:58 +0900

    Create kadai6 "新規プロジェクトを作成しました"

---
+++ src/main/java/com/arias/kadai6/Kadai6Application.java
@@ -0,0 +1,13 @@
+package com.arias.kadai6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kadai6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kadai6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/arias/kadai6/Kadai6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arias.kadai6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kadai6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
